7c5927fae6d7313d07f86b4515416c0501020e0e
Kevin Yan <guanwenyan301@gmail.com>
2024-08-21 05:04:50 -0500

    initial website finished

---
+++ components/intro.tsx
@@ -0,0 +1,70 @@
+"use client";
+
+import Image from "next/image";
+import React from "react";
+import { motion } from "framer-motion";
+import Link from "next/link";
+import { BsArrowRight, BsLinkedin } from "react-icons/bs";
+import { HiDownload } from "react-icons/hi";
+import { FaGithubSquare } from "react-icons/fa";
+import { useSectionInView } from "@/lib/hooks";
+import { useActiveSectionContext } from "@/context/active-section-context";
+
+export default function Intro() {
+  const { ref } = useSectionInView("Home", 0.5);
+  const { setActiveSection, setTimeOfLastClick } = useActiveSectionContext();
+
+  return (
+    <section
+      ref={ref}
+      id="home"
+      className="mb-28 max-w-[50rem] text-center sm:mb-0 scroll-mt-[100rem]"
+    >
+
+      <motion.h1
+        className="mb-10 mt-4 px-4 text-2xl font-medium !leading-[1.5] sm:text-4xl"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+      >
+        <span className="font-bold">Hello, I'm Guanwen Yan.</span> I'm a{" "}
+        senior studying CS & Stats at UIUC. I enjoy building {" "}
+        <span className="italic">sites & apps</span>
+      </motion.h1>
+
+      <motion.div
+        className="flex flex-col sm:flex-row items-center justify-center gap-2 px-4 text-lg font-medium"
+        initial={{ opacity: 0, y: 100 }}
+        animate={{ opacity: 1, y: 0 }}
+        transition={{
+          delay: 0.1,
+        }}
+      >
+
+        <a
+          className="group bg-white px-7 py-3 flex items-center gap-2 rounded-full outline-none focus:scale-110 hover:scale-110 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10"
+          href="/Guanwen Yan Resume.pdf"
+          download
+        >
+          Download Resume{" "}
+          <HiDownload className="opacity-60 group-hover:translate-y-1 transition" />
+        </a>
+
+        <a
+          className="bg-white p-4 text-gray-700 hover:text-gray-950 flex items-center gap-2 rounded-full focus:scale-[1.15] hover:scale-[1.15] active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://www.linkedin.com/in/guanwenyan/"
+          target="_blank"
+        >
+          <BsLinkedin />
+        </a>
+
+        <a
+          className="bg-white p-4 text-gray-700 flex items-center gap-2 text-[1.35rem] rounded-full focus:scale-[1.15] hover:scale-[1.15] hover:text-gray-950 active:scale-105 transition cursor-pointer borderBlack dark:bg-white/10 dark:text-white/60"
+          href="https://github.com/KevinYan12138"
+          target="_blank"
+        >
+          <FaGithubSquare />
+        </a>
+      </motion.div>
+    </section>
+  );
+}
---
+++ lib/data.ts
@@ -0,0 +1,82 @@
+import React from "react";
+import { CgWorkAlt } from "react-icons/cg";
+import { FaReact } from "react-icons/fa";
+import { LuGraduationCap } from "react-icons/lu";
+import corpcommentImg from "@/public/corpcomment.png";
+import rmtdevImg from "@/public/rmtdev.png";
+import wordanalyticsImg from "@/public/wordanalytics.png";
+
+export const links = [
+  {
+    name: "Home",
+    hash: "#home",
+  },
+  {
+    name: "About",
+    hash: "#about",
+  },
+  {
+    name: "Projects",
+    hash: "#projects",
+  },
+  {
+    name: "Skills",
+    hash: "#skills",
+  },
+  {
+    name: "Experience",
+    hash: "#experience",
+  },
+] as const;
+
+export const experiencesData = [
+  {
+    title: "Software Engineer Intern",
+    company: "Corteva Agriscience",
+    description:
+      "Developed a web application to replace OutSystems app for over 100 research facilities, projecting annual savings of over $100,000 and thousands of work hours in global research operations",
+    icon: React.createElement(CgWorkAlt),
+    date: "May 2024 - August 2024",
+  },
+  {
+    title: "Software Engineer Intern",
+    company: "Corteva Agriscience",
+    description:
+      "Reduced latency by more than 85% (from 10s to 1.2s) for REST API calls by optimizing the automated system that captures and uploads satellite map screenshots",
+    icon: React.createElement(CgWorkAlt),
+    date: "May 2023 - August 2023",
+  },
+  {
+    title: "Software Engineer Intern",
+    company: "University of Illinois Urbana-Champaign",
+    description:
+      "Developed an English learning Android App using Android Studio for Costa Rica customs with two professors and five students, empowering officers to effectively assist travelers from diverse backgrounds worldwide",
+    icon: React.createElement(CgWorkAlt),
+    date: "February 2022 - May 2022",
+  },
+] as const;
+
+export const projectsData = [
+] as const;
+
+export const skillsData = [
+  "Java",
+  "Python",
+  "C++",
+  "C#",
+  ".NET",
+  "Spring Boot",
+  "Flask",
+  "Angular",
+  "TypeScript",
+  "JavaScript",
+  "React",
+  "Azure",
+  "AWS",
+  "Azure",
+  "Node.js",
+  "MySQL",
+  "MongoDB",
+  "GraphQL",
+  "Rest API",
+] as const;
